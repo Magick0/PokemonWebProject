eb443b95041a52d1ee5e361156f1481081def7c3
Titouan LR <tlouisre56730@gmail.com>
2026-03-18 08:25:16 +0100

    tst

---
+++ TODO.md
@@ -0,0 +1,10 @@
+type_effectiveness.js
+
+|
+v
+
+proposez une classe Type qui permettra de
+connaître l’efficacité d’un type d’attaque contre un type de défenseur. Pour chaque type,
+l’efficacité devra être un objet littéral dont les attributs sont les noms des types et les
+valeurs associées sont le taux d’efficacité. Écrivez une méthode toString() synthétique
+qui retourne une chaîne (sans \n) contenant :
--- type.js
+++ type.js
@@ -2,8 +2,44 @@ class Type{
 
     static all_type;
 
-    constructor() {
 
+    constructor(nom, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18){
+        this.nomType = nom
+        this.Bug = t1
+        this.Dark = t2
+        this.Dragon = t3
+        this.Elecrtic = t4
+        this.Fairy = t5
+        this.Fighting = t6
+        this.Fire = t7
+        this.Flying = t8
+        this.Ghost= t9
+        this.Grass = t10
+        this.Ground = t11
+        this.Ice = t12
+        this.Normal = t13
+        this.Poison = t14
+        this.Psychic = t15
+        this.Rock = t16
+        this.Steel = t17
+        this.Water = t18
     }
 
+    triTst(){
+        let tabTypes = [];
+        for (let i = 1; i < 19; i++) {
+            tabTypes.push(this['t' + i])
+        }
+        console.table(tabTypes);
+    }
+
+    toString(){
+        return '${this.nomType} : ${this.Bug} = []'}
+
+    
+
 }
+
+const bug = new Type("Bug", 1, 2, 3, 4, 5, 6,1 ,1 , 1, 1,1 ,1 ,1 ,1 ,1, 1, 1, 1);
+
+bug.triTst();
