afe54f49883b0aec09ad2368a4b3c5f3fafe68ba
Titouan <tlouisre56730@gmail.com>
2026-03-20 11:08:16 +0100

    tst

--- class_pokemon.js
+++ class_pokemon.js
@@ -6,23 +6,23 @@ class Pokemon {
         this.form = form;
         this.pokemon_id = pokemon_id;
         this.pokemon_name = pokemon_name;
+        this.types = this.getTypes();
     }
 
     toString() {
-        return `${this.pokemon_name} : #${this.pokemon_id}, [type], [STA: ${this.base_stamina}, ATK: ${this.base_attack}, DEF: ${this.base_defense}], Rapides = [], Chargées = []`;
+        return `${this.pokemon_name} : #${this.pokemon_id}, [${this.types.type.join(", ")}], [STA: ${this.base_stamina}, ATK: ${this.base_attack}, DEF: ${this.base_defense}], Rapides = [], Chargées = []`;
     }
-
+    
+    getTypes() {
+        const typeInfo = pokemon_types.find(pt => 
+            pt.pokemon_id === this.pokemon_id && pt.form === this.form
+        );
+        // console.table(typeInfo.type);
+        return typeInfo;
+    }
 }
 
-getTypes() {
-    // Chercher dans pokemon_types le type qui correspond à cet ID et cette form
-    const typeInfo = pokemon_types.find(pt => 
-        pt.pokemon_id === this.pokemon_id && pt.form === this.form
-    );
-    return typeInfo ? typeInfo.type : [];
-}
 
-
-
-Bulbasaur : #1, [Normal], [STA: 155, ATK: 118, DEF: 111], Rapides = 
-[Vine Whip, Tackle], Chargées = [Sludge Bomb, Seed Bomb, Power Whip] 
+const bulbasaur = new Pokemon(118, 111, 128, "Normal", 1, "Bulbasaur");
+bulbasaur.getTypes();
+console.table(bulbasaur.toString());
