08eb35e0b32c318f4dcfbd58defc25266cf061cd
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-03-25 07:56:44 +0100

    test

--- testTempo.js
+++ testTempo.js
@@ -1,11 +1,13 @@
 function getPokemonsByType(typeName){
     // afficher la liste des pokemon pour un type donnée
     for (const type of pokemon_types){
+        // si on retrouve le type 
         if(type.type.includes(typeName)){
             const stats = pokemons.find(
                 p => p.pokemon_id === type.pokemon_id && p.form == type.form
             );
             if(stats){
+                // on créer un pokemon temporaire qu'on affiche
                 const p = new Pokemon(
                     stats.base_attack,
                     stats.base_defense,
@@ -23,12 +25,15 @@ function getPokemonsByType(typeName){
 function getPokemonsByAttack(attackName){
     // liste des pokémon pour une attaque donnée
     for (const attack of pokemon_moves){
+        // on liste les élement à parcourir
         moves = [...attack.charged_moves,...attack.elite_charged_moves,...attack.elite_fast_moves,...attack.fast_moves];
+        // si dans nos élement on retrouve l'attack recherché
         if(moves.includes(attackName)){
             const stats = pokemons.find(
                 p => p.pokemon_id === attack.pokemon_id && p.form == attack.form
             );
             if(stats){
+                // on créer un pokemon temporaire qu'on affiche
                 const p = new Pokemon(
                     stats.base_attack,
                     stats.base_defense,
@@ -42,9 +47,18 @@ function getPokemonsByAttack(attackName){
         }
     }
 }
+
 function getAttacksByType(typeName){
-    Type.fill_types();
-    console.table(all_type);
+    // On remplie all attack
+    Attack.fill_attacks();
+    // pour toutes les attack
+    for(const attack of Object.values(Attack.all_attacks)){
+        // si le type de l'attaque correspond à notre recherche
+        if(typeName === attack.type){
+            // on l'affiche
+            console.table(attack);
+        }
+    }
 }
 
 function sortPokemonByTypeThenName(){
