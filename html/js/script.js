6a606ae75d7464b5de03f299765bb4889975e15e
Titouan <tlouisre56730@gmail.com>
2026-04-05 17:30:45 +0200

    TODO Pagination

--- html/js/script.js
+++ html/js/script.js
@@ -1,3 +1,29 @@
 Attack.fill_attacks();
 Type.fill_types();
 Pokemon.fill_Pokemons()
+
+
+function displayPokemons(pokemonList) {
+    const tableBody = document.getElementById('pokemon-table-body');
+    pokemonList.forEach(pokemon => {
+        const row = document.createElement('tr');
+
+
+        row.innerHTML = `
+            <td>${pokemon.pokemon_id}</td>
+            <td>${pokemon.pokemon_name}</td>
+            <td>${pokemon.generation}</td>
+            <td>${pokemon.types.join(', ')}</td>
+            <td>${pokemon.base_stamina}</td>
+            <td>${pokemon.base_attack}</td>
+            <td>${pokemon.base_defense}</td>
+            <td><img src="webp/thumbnails/${String(pokemon.pokemon_id).padStart(3, '0')}.webp" alt="${pokemon.pokemon_name}"></td>
+        `;
+
+        tableBody.appendChild(row);
+    });
+}
+
+console.table(Pokemon.all_pokemons);
+
+displayPokemons(Pokemon.all_pokemons);
